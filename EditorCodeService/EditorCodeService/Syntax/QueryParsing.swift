115dbb8c97510f2c0919ce68f1dc3be404bd2001
nicolinoSt <sterchele.nicolas@gmail.com>
2016-04-18 19:48:48 +0200

    Remove files

--- EditorCodeService/EditorCodeService/Syntax/QueryParsing.swift
+++ EditorCodeService/EditorCodeService/Syntax/QueryParsing.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 
-protocol SyntaxParsing {
+protocol QueryParsing {
 	<#requirements#>
 }
--- EditorCodeService/EditorCodeService/SyntaxAnalyzer/SyntaxParsing.swift
+++
@@ -1,9 +0,0 @@
-//
-//  SyntaxParsing.swift
-//  EditorCodeService
-//
-//  Created by Nicolas Sterchele on 13/04/16.
-//  Copyright © 2016 nicolinoST. All rights reserved.
-//
-
-import Foundation
