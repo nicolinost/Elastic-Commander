1ad90f0b3547c06467e45e2e27f34b3436b8b9c2
nicolinoSt <sterchele.nicolas@gmail.com>
2016-05-04 21:44:19 +0200

    Continue to transfer swift code to C code on lexer program

--- Lexer.c
+++ Lexer.c
@@ -12,30 +12,95 @@ struct Token{
 };
 
 void ToString(struct Token * token);
-void LexRawLine(char &Ptr);
+void LexRawLine(char *Ptr);
+void LexIdentifier(char *Ptr);
 
 int main() {
 
   enum TokenType type = COMMA;
   struct Token t;
   t.TYPE = type;
-  t.VALUE = "TEST";
+  t.VALUE = "POST /kmcloud_percolator/.percolator/_search { } ";
   ToString(&t);
 
-  LexRawLine(&t.VALUE);
+  LexRawLine(t.VALUE);
   
 }
 
 
-void LexRawLine(char &Ptr){
+void LexRawLine(char *Ptr){
   
-  while(Ptr != '\0'){
-    printf("%c\n",Ptr);
-    Ptr++;
+  while(*Ptr != '\0'){
+
+     switch(*Ptr){
+
+     case '{':
+       printf("IS L_CURLY_BRACKET\n");
+       //Append new token with L_CURLY_BRACKET as type
+       break;
+     case '}':
+       printf("IS R_CURLY_BRACKET\n");
+     //Append new token with R_CURLY_BRACKET as type
+       break;
+     case '[':
+       printf("IS L_SQUARE_BRACKET\n");
+       //append new token with L_SQUARE_BRACKET as type
+       break;
+        case ']':
+       printf("IS R_SQUARE_BRACKET\n");
+       //append new token with R_SQUARE_BRACKET as type
+       break;
+     case ',':
+       printf("IS COMMA\n");
+       //append new token with COMMA as type
+       break;
+     case '\"':
+       printf("IS STRING_LITERAL\n");
+       //LexStringLiteral(*Ptr);
+       break;
+     case '/':
+       if((*Ptr)+1 != '_'){
+	 printf("IS ENDPOINT\n");
+	 //LexUrlEndPoint(*Ptr);
+       }else{
+	 printf("IS API\n");
+	 //LexUrlApi(*Ptr);
+       }
+       break;
+     case '_':
+       printf("IS UNDERSCORE\n");
+       //LexUrlApi(*Ptr);
+       break;
+     case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g':
+     case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+     case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+     case 'v': case 'w': case 'x': case 'y': case 'z':
+     case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G':
+     case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+     case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
+     case 'V': case 'W': case 'X': case 'Y': case 'Z':
+      printf("IS IDENTIFIER\n");
+      //LexIdentifier(*Ptr);
+      break;
+     case ':':
+       printf("IS COLON\n");
+       //append new token with TOKEN as type
+       break;
+      
+    }
+     *Ptr++;
   }
   
 }
 
+void LexIdentifier(char *Ptr){
+  
+  while(*Ptr!='\0'){
+    if(*Ptr == ' '){
+      return 
+    }
+  }
+}
 
 
 void ToString(struct Token * token){
